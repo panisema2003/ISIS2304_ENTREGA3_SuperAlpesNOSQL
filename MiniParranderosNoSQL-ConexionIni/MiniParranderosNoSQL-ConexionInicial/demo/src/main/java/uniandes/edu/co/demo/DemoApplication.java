aa35f673f59a8bc9a3223fa934ff67cfd62e143b
Nicolas Salazar <n.salazars@uniandes.edu.co>
2024-11-29 18:49:01 -0500

    creacion

---
+++ MiniParranderosNoSQL-ConexionIni/MiniParranderosNoSQL-ConexionInicial/demo/src/main/java/uniandes/edu/co/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package uniandes.edu.co.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DemoApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
