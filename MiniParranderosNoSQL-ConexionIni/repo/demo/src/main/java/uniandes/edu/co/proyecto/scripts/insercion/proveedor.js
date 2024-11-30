b74096688c33b90dea2c9a3490c8e0f65fb13629
Nicolas Salazar <n.salazars@uniandes.edu.co>
2024-11-30 10:18:52 -0500

    cambios

--- MiniParranderosNoSQL-ConexionIni/repo/demo/src/main/java/uniandes/edu/co/proyecto/scripts/insercion/proveedor.js
+++
@@ -1,8 +0,0 @@
-db.proveedor.insertOne({
-  id: 1,
-  NIT: 1234567890123,
-  direccion: "Calle 123, Bogotá, Colombia",
-  nombre_contacto: "Juan Pérez",
-  telefono_contacto: "3001234567",
-  id_productos: [101, 102, 103],
-});
